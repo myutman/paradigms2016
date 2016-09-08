03c0d2d00aed449cca95db11e9e72e861abd4954
Mikhail <myutman@gmail.com>
2016-09-08 13:48:51 +0300

    added string_task.py

---
+++ 08.09.16/string_task.py
@@ -0,0 +1,47 @@
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+#
+# Example input: 'read'
+# Example output: 'reading'
+def verbing(s):
+    if (len(s) < 3):
+        return s
+    if (s[len(s) - 3:len(s):] == 'ing'):
+        return s + 'ly' 
+    return s + 'ing'
+ 
+ 
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+#
+# Example input: 'This dinner is not that bad!'
+# Example output: 'This dinner is good!'
+def not_bad(s):
+    a = s.find('not')
+    b = s.find('bad')
+    if (a < b and a != -1 and b != -1):
+        s = s.replace(s[a:b + 3:], 'good')
+    return s
+ 
+ 
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+#
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+#
+# Example input: 'abcd', 'xy'
+# Example output: 'abxcdy'
+def front_back(a, b):
+    diva = (len(a) + 1) // 2 
+    divb = (len(b) + 1) // 2
+    return a[0:diva:] + b[0:divb:] + a[diva:len(a):] + b[divb:len(b):]
