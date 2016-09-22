f0c5f63c59e8e122a142802f1a07d913fad3108f
Mikhail <myutman@gmail.com>
2016-09-22 23:14:14 +0300

    fixed

--- homework2/find_duplicates.py
+++ homework2/find_duplicates.py
@@ -5,37 +5,39 @@ import argparse
 import os
 import re
 import hashlib
+import collections
 
-parser = argparse.ArgumentParser()
-parser.add_argument('top_dir', type=str, help='The name of top directory')
-args = parser.parse_args()
+def get_hash(z):
+    hasher = hashlib.sha1(z)
+    return hasher.hexdigest()        
 
-pattern = re.compile("^[.~]")
+def read_bytes(filename):
+    with open(filename, 'rb') as f:
+        stats = os.stat(filename)
+        return f.read(stats.st_size)
 
-dic = dict()
+def walk_through_directories(top_dir):
+    dic = collections.defaultdict(list)
+    for root, _, files in os.walk(top_dir):
+        for filename in files:
+            if (not pattern.match(filename)):
+                fullname = os.path.join(root, filename)
+                z = read_bytes(fullname)            
+                a = get_hash(z)
+                dic[a].append(os.path.relpath(fullname, top_dir))
+    return dic
 
-for root, dirs, files in os.walk(args.top_dir):
-    for filename in files:
-        if (not pattern.match(filename)):
-            fullname = root + ("\\" if (len(root) > 0) and root[-1] != "\\" else "") + filename
-            #print(fullname)
-            stats = os.stat(fullname)
-            with open(fullname, 'rb') as f:
-                z = f.read(1024)
-                #print(z)
-                hasher = hashlib.sha1(z)
-                a = hasher.hexdigest()
-                #print(a)
-                if (not a in dic):
-                    dic[a] = []
-                prefix = root[len(args.top_dir):]
-                dic[a].append(prefix + ("\\" if (len(prefix) > 0 and prefix[-1] != "\\") else "") + filename)
+def output(dic):
+    for hsh in dic:
+        if len(dic[hsh]) > 1:
+            print(":".join(dic[hsh]))
 
-for hsh in dic:
-    if len(dic[hsh]) > 1:
-        for a in dic[hsh]:
-            print(a, end=(":" if a != dic[hsh][-1] else "\n"))
-
-            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('top_dir', type=str, help='The name of top directory')
+    args = parser.parse_args()
+    pattern = re.compile("^[.~]")
+    dic = walk_through_directories(args.top_dir)        
+    output(dic)            
             
     
