593f194f02386c3ba273de07fd665572ce897c65
myutman <myutman@gmail.com>
2016-10-15 02:23:14 +0300

    done printer

---
+++ homework5/printer.py
@@ -0,0 +1,72 @@
+class PrettyPrinter:
+
+    def __init__(self):
+        self.b = False
+
+    def visit(self, tree, tabs):
+        b = self.b
+        self.b = True
+        name = tree.__class__.__name__
+        fn = getattr(self, 'visit' + name)
+        fn(tree, tabs)
+        self.b = b
+        if (not b):
+            print(";")
+
+    def visitNumber(self, num, tabs):
+        print("\t" * tabs + str(num.value), end="")
+
+    def visitFunctionDefinition(self, fun, tabs):
+        print("\t" * tabs + "def " + fun.name + " (" + ", ".join(fun.function.args) + ") {")
+        for var in fun.function.body:
+            self.visit(var, tabs + 1)
+            print(";")
+        print("\t" * tabs, end="}")
+
+    def visitConditional(self, cond, tabs):
+        print("\t" * tabs, end = "if (")
+        self.visit(cond.condition, 0)
+        print(") {")
+        for var in cond.if_true:
+            self.visit(var, tabs + 1)
+            print(";")
+        if len(cond.if_false):
+            print("\t" * tabs + "} else {")
+            for var in cond.if_false:
+                self.visit(var, tabs + 1)
+                print(";")
+        print("\t" * tabs, end="}")
+
+    def visitPrint(self, prt, tabs):
+        print("\t" * tabs, end="print(")
+        self.visit(prt.expr, 0)
+        print(end=")")
+
+    def visitRead(self, rd, tabs):
+        print("\t" * tabs + "read(" + rd.name, end=")")
+
+    def visitFunctionCall(self, fun, tabs):
+        self.visit(fun.fun_expr, tabs)
+        print(end="(")
+        if len(fun.args):
+            self.visit(fun.args[0], 0)
+            for var in fun.args[1:]:
+                 print(end=", ")
+                 self.visit(var, 0)
+        print(end=")")
+        
+    def visitReference(self, ref, tabs):
+        print("\t"*tabs, end=ref.name)
+
+    def visitBinaryOperation(self, bnr, tabs):
+        print("\t"*tabs, end="(")
+        self.visit(bnr.lhs, 0)
+        print(") " + bnr.op ,end=" (")
+        self.visit(bnr.rhs, 0)
+        print(end=")")
+
+    def visitUnaryOperation(self, unr, tabs):
+        print(unr.op, end="(")
+        self.visit(unr.expr, 0)
+        print(end=")")
+
---
+++ homework5/yat.py
@@ -0,0 +1,189 @@
+import printer
+
+class Scope:
+
+    def __init__(self, parent=None):
+        self.d = dict()
+        self.parent = parent
+
+    def __getitem__(self, key):
+
+        if (not key in self.d):
+            return self.parent[key] if self.parent != None else None
+        return self.d[key]    
+
+    def __setitem__(self, key, value):
+        self.d[key] = value 
+
+class Number:
+
+    def __init__(self, value):
+        self.value = value
+
+    def evaluate(self, scope):
+        return self
+
+
+class Function:
+
+   
+    def __init__(self, args, body):
+        self.args = args
+        self.body = body
+
+    def evaluate(self, scope):
+        tmp = None
+        for var in self.body:
+            tmp = var.evaluate(scope)
+        return tmp
+
+
+class FunctionDefinition:
+
+    
+    def __init__(self, name, function):
+        self.name = name
+        self.function = function
+
+    def evaluate(self, scope):
+        scope[self.name] = self.function
+        return self.function
+
+
+class Conditional:
+
+    
+
+    def __init__(self, condition, if_true, if_false=None):
+        self.condition = condition
+        self.if_true = if_true
+        self.if_false = if_false
+
+    def evaluate(self, scope):
+        branch = self.if_true if self.condition.evaluate(scope).value else self.if_false
+        tmp = None
+        for var in branch:
+            tmp = var.evaluate(scope)
+        return tmp
+
+
+class Print:
+
+    
+    def __init__(self, expr):
+        self.expr = expr
+
+    def evaluate(self, scope):
+        print(self.expr.evaluate(scope).value)
+
+
+class Read:
+
+    
+
+    def __init__(self, name):
+        self.name = name
+
+    def evaluate(self, scope):
+        tmp = Number(int(input()))
+        scope[self.name] = tmp
+        return tmp
+
+
+class FunctionCall:
+
+    
+
+    def __init__(self, fun_expr, args):
+        self.fun_expr = fun_expr
+        self.args = args
+
+    def evaluate(self, scope):
+        function = self.fun_expr.evaluate(scope)
+        call_scope = Scope(scope)
+        lst = [] 
+        for name, arg in zip(function.args, self.args):
+            call_scope[name] = arg.evaluate(scope)
+        return function.evaluate(call_scope)
+
+
+
+class Reference:
+
+    
+    def __init__(self, name):
+        self.name = name
+
+    def evaluate(self, scope):
+        return scope[self.name]
+
+
+class BinaryOperation:
+
+    
+
+    def __init__(self, lhs, op, rhs):
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+        self.d = {'+': (lambda x,y: x + y), '*': (lambda x,y: x * y), '-': (lambda x,y: x - y), '/': (lambda x,y: x // y),
+                  '%': (lambda x,y: x % y), '==': (lambda x,y: x == y), '!=': (lambda x,y: x != y), '<': (lambda x,y: x < y),
+                  '>': (lambda x,y: x > y), '<=': (lambda x,y: x <= y), '>=': (lambda x,y: x >= y), '&&': (lambda x,y: x and y), '||': (lambda x,y: x or y)}
+
+    def evaluate(self, scope):
+        lhs = self.lhs.evaluate(scope)
+        rhs = self.rhs.evaluate(scope)
+        return Number(self.d[self.op](lhs.value, rhs.value))
+
+
+class UnaryOperation:
+
+    
+    def __init__(self, op, expr):
+        self.op = op
+        self.expr = expr
+        self.d = {'-': (lambda x: -x), '!': (lambda x: not x)}
+
+    def evaluate(self, scope):
+        expr = self.expr.evaluate(scope)
+        return Number(self.d[self.op](expr.value))
+       
+
+
+
+def example():
+    parent = Scope()
+    parent["foo"] = Function(('hello', 'world'),
+                             [Print(BinaryOperation(Reference('hello'),
+                                                    '+',
+                                                    Reference('world')))])
+    parent["bar"] = Number(10)
+    scope = Scope(parent)
+    assert 10 == scope["bar"].value
+    scope["bar"] = Number(20)
+    assert scope["bar"].value == 20
+    print('It should print 2: ', end=' ')
+    a = FunctionCall(Reference('foo'),
+                 [Number(5), UnaryOperation('-', Number(3))])
+    a.evaluate(scope)
+    ppt = printer.PrettyPrinter()
+    ppt.visit(a, 0)
+
+def my_tests():
+    abacaba = Scope()
+    a = FunctionDefinition("divisor", Function(('n', 'i'), [Conditional(BinaryOperation(BinaryOperation(Reference('i'), '*', Reference('i')), '>', Reference('n')),
+                                 [Number(0)], [Conditional(BinaryOperation(Reference('n'), '%', Reference('i')), [FunctionCall(Reference("divisor"), [Reference('n'),
+                                 BinaryOperation(Reference('i'), '+', Number(1))])], [Number(1)])])]))
+    b = FunctionDefinition("next_prime", Function(('n'), [a, Conditional(FunctionCall(Reference("divisor"), [Reference('n'), Number(2)]),
+                                    [FunctionCall(Reference("next_prime"), [BinaryOperation(Reference('n'), '+', Number(1))])], [Reference('n')])]))
+    c = FunctionDefinition("main", Function([], [b, Print(FunctionCall(Reference("next_prime"), [Read("tmp")]))]))
+    d = FunctionCall(c, [])
+    d.evaluate(abacaba)
+    ppt = printer.PrettyPrinter()
+    print("\n")
+    ppt.visit(d, 0)
+    print()
+
+if __name__ == '__main__':
+    example()
+    my_tests()
