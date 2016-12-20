d0c772f896a2479a118ea06b55761839eeac6788
myutman <myutman@gmail.com>
2016-12-21 01:16:55 +0300

    fixed

--- homework11/my_test.py
+++ homework11/my_test.py
@@ -1,37 +1,79 @@
-from unittest.mock import patch
+from unittest.mock import *
+from io import StringIO
 import sys
 import io
 from model import *
 
+@patch('sys.stdout', new_callable=StringIO)
+def compare(expr, val, mock_print):
+	scope = Scope()
+	Print(expr).evaluate(scope)
+	assert mock_print.getvalue() == (str(val) + '\n')
+
+def test_print():
+	compare(Number(1), 1)
+
 def test_read():
 	with patch('sys.stdin', new=io.StringIO('42')):
 		r = Read('a')
-		s = Scope()
-		assert BinaryOperation(r, '+', Reference('a')).evaluate(s).value == 84
+		compare(r, 42)
 
 def test_bo():
-	s = Scope()
-	assert BinaryOperation(Number(1), '+', Number(1)).evaluate(s).value == 2
-	assert BinaryOperation(Number(1), '==', Number(1)).evaluate(s).value == 1
-	assert BinaryOperation(Number(1), '!=', Number(1)).evaluate(s).value == 0
+	compare(BinaryOperation(Number(1), '+', Number(1)), 2)
+	compare(BinaryOperation(Number(3), '-', Number(1)), 2)
+	compare(BinaryOperation(Number(2), '*', Number(1)), 2)
+	compare(BinaryOperation(Number(1), '==', Number(1)), True)
+	compare(BinaryOperation(Number(1), '!=', Number(1)), False)
+
+def test_uo():
+	compare(UnaryOperation('-', Number(1)), -1)
+	compare(UnaryOperation('!', Number(2)), False)
 
 def test_cond():
-	s = Scope()
-	assert Conditional(BinaryOperation(Number(1), '+', Number(1)), [Number(2)], [Number(3)]).evaluate(s).value == 2
-	assert Conditional(BinaryOperation(Number(1), '-', Number(1)), [Number(2)], [Number(3)]).evaluate(s).value == 3
-	assert Conditional(BinaryOperation(Number(1), '-', Number(1)), [Number(2)]).evaluate(s) == None
+	compare(Conditional(Number(1), [Number(2)], [Number(3)]), 2)
+	compare(Conditional(Number(0), [Number(2)], [Number(3)]), 3)
+	compare(Conditional(Number(1), [Number(2)]), 2)
+
+def test_ref():
+	scope = Scope()
+	scope['a'] = Number(3)
+	compare(Reference('a').evaluate(scope), 3)
 
 def test_scope():
 	par = Scope()
 	par['a'] = Number(1)
 	sc = Scope(par)
-	par['b'] = Number(2)
-	assert BinaryOperation(Reference('a'), '+', Reference('b')).evaluate(sc).value == 3
-	#assert BinaryOperation(Reference('a'), '+', Reference('c')).evaluate(sc).value == 3
+	sc['b'] = Number(2)
+	compare(BinaryOperation(Reference('a'), '+', Reference('b')).evaluate(sc), 3)
+
+def test_func():
+	#Example
+	parent = Scope()
+	parent["bar"] = Number(10)
+	scope = Scope(parent)
+	parent["foo"] = Function(('hello', 'world'), [BinaryOperation(Reference('hello'), '+', Reference('world'))])
+	compare(FunctionCall(FunctionDefinition('foo', parent['foo']), [Number(2), UnaryOperation('-', Number(3))]).evaluate(scope), -1)
+
+def test_my():
+	with patch('sys.stdin', new=io.StringIO('42')):
+		abacaba = Scope()
+		abacaba["divisor"] = Function(('n', 'i'), [Conditional(BinaryOperation(BinaryOperation(Reference('i'), '*', Reference('i')), '>', Reference('n')),
+	    	                         [Number(0)], [Conditional(BinaryOperation(Reference('n'), '%', Reference('i')), [FunctionCall(Reference("divisor"), [Reference('n'),
+        	                         BinaryOperation(Reference('i'), '+', Number(1))])], [Number(1)])])])
+		abacaba["next_prime"] = Function(('n'), [Conditional(FunctionCall(Reference("divisor"), [Reference('n'), Number(2)]),
+        	                            [FunctionCall(Reference("next_prime"), [BinaryOperation(Reference('n'), '+', Number(1))])], [Reference('n')])])
+		abacaba["main"] = Function([], [FunctionCall(Reference('next_prime'), [Read("tmp")])])
+		compare(FunctionCall(Reference("main"), []).evaluate(abacaba), 43)
+
+
 
 if (__name__ == "__main__"):
 	test_bo()
+	test_uo()
+	test_func()
 	test_scope()
 	test_cond()
 	test_read()
-
+	test_my()
+	test_ref()
+	test_print()
