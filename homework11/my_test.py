a6437d170351939c91cc0f9d57ae63f62a86219f
myutman <myutman@gmail.com>
2016-12-21 14:47:38 +0300

    fixed

--- homework11/my_test.py
+++ homework11/my_test.py
@@ -1,4 +1,5 @@
 from unittest.mock import *
+from unittest import TestCase
 from io import StringIO
 import sys
 import io
@@ -8,72 +9,92 @@ from model import *
 def compare(expr, val, mock_print):
 	scope = Scope()
 	Print(expr).evaluate(scope)
-	assert mock_print.getvalue() == (str(val) + '\n')
+	return mock_print.getvalue() == (str(val) + '\n')
 
-def test_print():
-	compare(Number(1), 1)
+class PrintTest(TestCase):
+	def test_print(self):
+		assert compare(Number(1), 1)
+		
+	def test_print1(self):
+		assert compare(Number(3), 3)
+	
+class ReadTest(TestCase):
+	def test_read(self):
+		with patch('sys.stdin', new=io.StringIO('42')):
+			r = Read('a')
+			assert compare(r, 42)
 
-def test_read():
-	with patch('sys.stdin', new=io.StringIO('42')):
-		r = Read('a')
-		compare(r, 42)
+class BinaryOperationTest(TestCase):
+	def test_add(self):
+		assert compare(BinaryOperation(Number(1), '+', Number(1)), 2)
+	
+	def test_sub(self):
+		assert compare(BinaryOperation(Number(3), '-', Number(1)), 2)
+	
+	def test_mult(self):
+		assert compare(BinaryOperation(Number(2), '*', Number(1)), 2)
+		
+	def test_eq(self):
+		assert (not compare(BinaryOperation(Number(1), '==', Number(1)), False)) and (not compare(BinaryOperation(Number(1), '==', Number(1)), 0))
+	
+	def test_neq(self):
+		assert compare(BinaryOperation(Number(1), '!=', Number(1)), False) or compare(BinaryOperation(Number(1), '!=', Number(1)), 0)
 
-def test_bo():
-	compare(BinaryOperation(Number(1), '+', Number(1)), 2)
-	compare(BinaryOperation(Number(3), '-', Number(1)), 2)
-	compare(BinaryOperation(Number(2), '*', Number(1)), 2)
-	compare(BinaryOperation(Number(1), '==', Number(1)), True)
-	compare(BinaryOperation(Number(1), '!=', Number(1)), False)
+class UnaryOperationTest(TestCase):
 
-def test_uo():
-	compare(UnaryOperation('-', Number(1)), -1)
-	compare(UnaryOperation('!', Number(2)), False)
+	def test_neg(self):
+		assert compare(UnaryOperation('-', Number(1)), -1)
+	
+	def test_not(self):
+		assert compare(UnaryOperation('!', Number(2)), False)
 
-def test_cond():
-	compare(Conditional(Number(1), [Number(2)], [Number(3)]), 2)
-	compare(Conditional(Number(0), [Number(2)], [Number(3)]), 3)
-	compare(Conditional(Number(1), [Number(2)]), 2)
+class ConditionalTest(TestCase):
 
-def test_ref():
-	scope = Scope()
-	scope['a'] = Number(3)
-	compare(Reference('a').evaluate(scope), 3)
+	def test_true(self):
+		assert compare(Conditional(Number(1), [Number(2)], [Number(3)]), 2)
+	
+	def test_false(self):
+		assert compare(Conditional(Number(0), [Number(2)], [Number(3)]), 3)
+	
+	def test_empty(self):
+		assert compare(Conditional(Number(1), [Number(2)]), 2)
 
-def test_scope():
-	par = Scope()
-	par['a'] = Number(1)
-	sc = Scope(par)
-	sc['b'] = Number(2)
-	compare(BinaryOperation(Reference('a'), '+', Reference('b')).evaluate(sc), 3)
+class ReferenceTest(TestCase):
 
-def test_func():
-	#Example
-	parent = Scope()
-	parent["bar"] = Number(10)
-	scope = Scope(parent)
-	parent["foo"] = Function(('hello', 'world'), [BinaryOperation(Reference('hello'), '+', Reference('world'))])
-	compare(FunctionCall(FunctionDefinition('foo', parent['foo']), [Number(2), UnaryOperation('-', Number(3))]).evaluate(scope), -1)
+	def test_ref(self):
+		scope = Scope()
+		scope['a'] = Number(3)
+		assert compare(Reference('a').evaluate(scope), 3)
 
-def test_my():
-	with patch('sys.stdin', new=io.StringIO('42')):
-		abacaba = Scope()
-		abacaba["divisor"] = Function(('n', 'i'), [Conditional(BinaryOperation(BinaryOperation(Reference('i'), '*', Reference('i')), '>', Reference('n')),
-	    	                         [Number(0)], [Conditional(BinaryOperation(Reference('n'), '%', Reference('i')), [FunctionCall(Reference("divisor"), [Reference('n'),
-        	                         BinaryOperation(Reference('i'), '+', Number(1))])], [Number(1)])])])
-		abacaba["next_prime"] = Function(('n'), [Conditional(FunctionCall(Reference("divisor"), [Reference('n'), Number(2)]),
-        	                            [FunctionCall(Reference("next_prime"), [BinaryOperation(Reference('n'), '+', Number(1))])], [Reference('n')])])
-		abacaba["main"] = Function([], [FunctionCall(Reference('next_prime'), [Read("tmp")])])
-		compare(FunctionCall(Reference("main"), []).evaluate(abacaba), 43)
+class ScopeTest(TestCase):
 
+	def test_scope(self):
+		par = Scope()
+		par['a'] = Number(1)
+		sc = Scope(par)
+		sc['b'] = Number(2)
+		assert compare(BinaryOperation(Reference('a'), '+', Reference('b')).evaluate(sc), 3)
 
+class FunctionTest(TestCase):
 
-if (__name__ == "__main__"):
-	test_bo()
-	test_uo()
-	test_func()
-	test_scope()
-	test_cond()
-	test_read()
-	test_my()
-	test_ref()
-	test_print()
+	def test_func(self):
+		#Example
+		parent = Scope()
+		parent["bar"] = Number(10)
+		scope = Scope(parent)
+		parent["foo"] = Function(('hello', 'world'), [BinaryOperation(Reference('hello'), '+', Reference('world'))])
+		assert compare(FunctionCall(FunctionDefinition('foo', parent['foo']), [Number(2), UnaryOperation('-', Number(3))]).evaluate(scope), -1)
+
+class AllTest(TestCase):
+
+	def test_my(self):
+		with patch('sys.stdin', new=io.StringIO('42')):
+			abacaba = Scope()
+			abacaba["divisor"] = Function(('n', 'i'), [Conditional(BinaryOperation(BinaryOperation(Reference('i'), '*', Reference('i')), '>', Reference('n')),
+		                                 [Number(0)], [Conditional(BinaryOperation(Reference('n'), '%', Reference('i')), [FunctionCall(Reference("divisor"), [Reference('n'),
+    	                                 BinaryOperation(Reference('i'), '+', Number(1))])], [Number(1)])])])
+			abacaba["next_prime"] = Function(('n'), [Conditional(FunctionCall(Reference("divisor"), [Reference('n'), Number(2)]),
+    	                                    [FunctionCall(Reference("next_prime"), [BinaryOperation(Reference('n'), '+', Number(1))])], [Reference('n')])])
+			abacaba["main"] = Function([], [FunctionCall(Reference('next_prime'), [Read("tmp")])])
+			assert compare(FunctionCall(Reference("main"), []).evaluate(abacaba), 43)
+
