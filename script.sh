933191852dfaafc9bf6b0eb3d204d27acf88fd14
root <root@lubuntu-VirtualBox.(none)>
2017-05-23 00:56:09 +0200

    fixed

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 pvcreate -ff /dev/sdb
-vgcreate vg00 /dev/sdb
+vgchange -a n my_vgroup
+vgremove my_vgroup
+vgchange -a y my_vgroup
+vgcreate my_vgroup /dev/sdb
 s=$(lsblk | sed -n -r -e 's/.*(sda[0-9]).* ([\.0-9]+)([A-Z]).*$/\1 \2 \3/p')
 i=0
 for a in $s; do
@@ -12,7 +15,7 @@ for a in $s; do
 		sz=$(bc <<< "$a * 1.5")
 	fi
 	if [ $i -eq 2 ]; then
-		lvcreate vg00 -n $name -L$sz$a
+		lvcreate my_vgroup -n $name -L$sz$a
 	fi
 	i=$(bc <<< "($i + 1) % 3");
 done
